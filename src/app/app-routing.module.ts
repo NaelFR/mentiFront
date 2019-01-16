bd4db2507f82c7bf3c03f3879873b28138664782
Nael <abou.chahine.nael@gmail.com>
2019-01-16 18:45:59 +0100

    add: routes definition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SessionComponent } from './session/session.component';
+import {AdminComponent} from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '',
+  component: HomeComponent,
+  },
+  {
+    path: 'session/:id',
+    component: SessionComponent,
+  },
+  {
+    path: 'admin/:id',
+    component: AdminComponent,
+  },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
