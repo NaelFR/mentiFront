62e632d6745cf35f0243f5a9605df20446b2f1d9
Nael <abou.chahine.nael@gmail.com>
2019-01-16 18:56:16 +0100

    changed: app component and app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ApiCallsService} from './api-calls.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'menti-front';
+export class AppComponent implements OnInit{
+  title = 'Menti-Nael';
+  questions;
+  sessions;
+
+  constructor(private apiCallsService: ApiCallsService) {}
+
+  ngOnInit() {
+    // this.getQuestions();
+    // this.getSessions();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+import { SessionComponent } from './session/session.component';
+import { AdminComponent } from './admin/admin.component';
+import { UsersResponsesGraphComponent } from './users-responses-graph/users-responses-graph.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SessionComponent,
+    AdminComponent,
+    UsersResponsesGraphComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
